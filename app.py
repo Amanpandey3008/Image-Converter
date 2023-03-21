169a585437c676728a8ef37938e37ee0edd36db0
Amanpandey3008 <94338123+Amanpandey3008@users.noreply.github.com>
2023-03-21 12:20:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 from flask import Flask, request, render_template
 from PIL import Image
+import io
+import base64
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        # Get the uploaded file
+        file = request.files['file']
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    # Get the uploaded file
-    file = request.files['file']
+        # Check which operation the user wants to perform
+        operation = request.form.get('operation')
 
-    # Open the image using PIL
-    img = Image.open(file)
+        # Open the image using PIL
+        img = Image.open(file)
 
-    # Convert the image to grayscale
-    img = img.convert('L')
+        # Perform the selected operation on the image
+        if operation == 'grayscale':
+            # Convert the image to grayscale
+            processed_img = img.convert('L')
+        elif operation == 'half_size':
+            # Resize the image to half of its size
+            new_width = int(img.width / 2)
+            new_height = int(img.height / 2)
+            processed_img = img.resize((new_width, new_height))
+        elif operation == 'quarter_size':
+            # Resize the image to quarter of its size
+            new_width = int(img.width / 4)
+            new_height = int(img.height / 4)
+            processed_img = img.resize((new_width, new_height))
 
-    # Save the grayscale image
-    img.save('static/grayscale.jpg')
+        # Save the processed image to memory buffer
+        buffer = io.BytesIO()
+        processed_img.save(buffer, format='JPEG')
+        buffer.seek(0)
 
-    # Render the result page
-    return render_template('result.html')
+        # Encode the processed image as base64 string
+        processed_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+        # Render the result page with the processed image
+        return render_template('index.html', processed_img=processed_base64)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
